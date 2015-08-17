528c489fa54d2d3f4fb701dcd7c4ab1e015930dc
Andy Stratton <theandystratton@gmial.com>
2015-08-17 13:26:52 -0400

    Updates.

--- functions.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-// Sizeable Core Functionality
-foreach ( glob( dirname( __FILE__ ) . '/core/*.php' ) as $file ) include $file;
-
-// Theme Specific Functionality
-foreach ( glob( dirname( __FILE__ ) . '/lib/*.php' ) as $file ) include $file;
